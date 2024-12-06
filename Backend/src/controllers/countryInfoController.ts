131b1dbb28e3d24c2023bc6b072ce9800c791dfc
Leandro <leandroviscolungo@gmail.com>
2024-12-06 14:03:20 -0300

    countryInfoController.ts completed

--- Backend/src/controllers/countryInfoController.ts
+++ Backend/src/controllers/countryInfoController.ts
@@ -8,14 +8,20 @@ export const countryInfoController = async (req: Request, res: Response) => {
 
         const responseFlags = await fetch('https://countriesnow.space/api/v0.1/countries/flag/images');
         const dataFlags = await responseFlags.json();
-        const flagData = dataFlags.data.find((flag: { iso2: string, iso3: string; }) => flag.iso2 === countryCode);
+        const flagData = dataFlags.data.find((x: { iso2: string; }) => x.iso2 === countryCode);
         
         const countryIso3 = flagData.iso3;
 
-        res.json({...dataCountryInfo, countryIso3: countryIso3});
+        const responsePopulation = await fetch('https://countriesnow.space/api/v0.1/countries/population/');
+        const dataPopulation = await responsePopulation.json();
+        const populationData = dataPopulation.data.find((x: { iso3: string; }) => x.iso3 === countryIso3);
+
+        res.json({...dataCountryInfo, flagData: flagData, population: populationData});
+        
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 };
+
 
 
